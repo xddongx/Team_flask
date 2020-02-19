9268e5721f7ba25675c3804410ad8fa4fc8cc407
xddongx <rlaehdugs21@hanmail.net>
2020-02-19 19:27:23 +0900

    emotion

---
+++ emotions.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+
+music = pd.read_csv('./static/csv/music.csv', header=None)
+
+music.columns = ['emotion', 'name', 'title', 'url']
+
+emotions = music.emotion.unique()
+
+def emotion(e):
+    if e == '불안한':
+        emo = emotions[0]
+    elif e == '화난':
+        emo = emotions[1]
+    elif e == '역겨운':
+        emo = emotions[2]
+    elif e == '행복한':
+        emo = emotions[3]
+    elif e == '슬픈':
+        emo = emotions[5]
+    elif e == '놀란':
+        emo = emotions[6]
+    else:
+        emo = emotions[4]
+
+    emotion_ = music[music.emotion == emo]
+    rand_list = emotion_.sample()
+    return rand_list
+
+text = '무표정'
+
+
+def emo(t):
+    list = emotion(t).values[0]
+    return list
+# print(list[0])
+# print(list[1])
+# print(list[2])
--- searchfile.py
+++ searchfile.py
@@ -1,11 +1,15 @@
 import os
+import emotions
 def search(dirname):
     filenames = os.listdir(dirname)
     list = []
     for filename in filenames:
         full_filename = os.path.join(filename)
         list.append(full_filename)
-    print(list)
+    # print(list)
     return list
 
 search("./static/image/member")
+
+test = emotions.emo('무표정')
+print(test)
